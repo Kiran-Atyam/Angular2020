6e4a71af9eba81ecffd801eefbca0b75857cc382
Kiran Atyam <k.atyam@zensar.com>
2022-04-26 12:21:12 +0530

    Array

---
+++ Basics/Arrayex.java
@@ -0,0 +1,26 @@
+public class Arrayex {
+    public static void main(String[] args) {
+        
+    
+    int[] arr = new int[5];
+    Object[] obj = new Object[2];
+    obj[0]="Kiran";
+    obj[1]=78;
+    for(Object ob:obj){
+        System.out.println(ob.toString());
+        
+    }
+
+    arr[0]=12;
+    arr[1]=13;
+    arr[2]=15;
+    arr[3]=20;
+    arr[4]=25;
+    int sum=1;
+    for(int  i :arr){
+        sum=sum+i;
+    }
+    System.out.println(sum);
+
+    }   
+}
