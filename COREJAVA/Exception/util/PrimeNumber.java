a5a2453d313a504905e7ea126fbbe07d467957cb
Kiran-Atyam <k.atyam@zensar.com>
2022-05-12 15:43:08 +0530

    Prime Number

---
+++ COREJAVA/Exception/util/PrimeNumber.java
@@ -0,0 +1,22 @@
+package Exception.util;
+
+public class PrimeNumber {
+    
+        public static boolean isPrime(int num) {
+            // 2 - (num-1)
+            for(int i = 2; i < num; i++) {
+                if(num % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static void main(String[] args) {
+            for (int i = 2; i <= 50; i++) {
+                if(isPrime(i)) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+
